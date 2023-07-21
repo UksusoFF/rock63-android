9a87a95112f301b8442488c3bb09a1ad6b23ffbe
Uksusov Kirill <k.uksusov@ya.ru>
2023-07-21 17:14:41 +0400

    Revert "Removed not used utility functions"
    This reverts commit 9188866b1d05c8c5ee6cffb13cf4361c43cc921e.

--- rock63/src/main/java/com/uksusoff/rock63/utils/CommonUtils.kt
+++ rock63/src/main/java/com/uksusoff/rock63/utils/CommonUtils.kt
@@ -9,6 +9,10 @@ object CommonUtils {
         return Date(ts.toLong() * 1000)
     }
 
+    fun getTimestampFromDate(d: Date?): Int {
+        return if (d == null) -1 else (d.time / 1000).toInt()
+    }
+
     fun getCroppedString(source: String, places: Int, addDots: Boolean): String {
         val postfix = if (addDots) "..." else ""
         return if (source.length < places - postfix.length) source else source.substring(0, places - postfix.length) + postfix
@@ -18,5 +22,31 @@ object CommonUtils {
         var noHTMLString = s.replace("<(.|\n)*?>".toRegex(), "")
         noHTMLString = noHTMLString.replace("&.*?;".toRegex(), "")
         return noHTMLString
+    }
+
+    @Throws(UnsupportedEncodingException::class)
+    fun convertStreamToString(`is`: InputStream): String {
+        val reader = BufferedReader(InputStreamReader(`is`, "UTF-8"))
+        val sb = StringBuilder()
+        var line: String? = null
+        try {
+            while (true) {
+                line = reader.readLine()
+                if (line == null) {
+                    break
+                }
+
+                sb.append(line)
+            }
+        } catch (e: IOException) {
+            e.printStackTrace()
+        } finally {
+            try {
+                `is`.close()
+            } catch (e: IOException) {
+                e.printStackTrace()
+            }
+        }
+        return sb.toString()
     }
 }
