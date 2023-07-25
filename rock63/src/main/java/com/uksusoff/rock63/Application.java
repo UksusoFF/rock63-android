4696a0f458f8af944e835c961ba5ff8febcd361d
Uksusov Kirill <k.uksusov@ya.ru>
2023-07-25 15:14:11 +0400

    Bugsnag integration added

--- rock63/src/main/java/com/uksusoff/rock63/Application.java
+++ rock63/src/main/java/com/uksusoff/rock63/Application.java
@@ -1,5 +1,6 @@
 package com.uksusoff.rock63;
 
+import com.bugsnag.android.Bugsnag;
 import com.evernote.android.job.JobManager;
 import com.uksusoff.rock63.jobs.JobCreator;
 
@@ -8,6 +9,8 @@ public class Application extends android.app.Application {
     @Override
     public void onCreate() {
         super.onCreate();
+
+        Bugsnag.start(this);
 
         JobManager.create(this).addJobCreator(new JobCreator());
     }
